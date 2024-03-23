f9952eb94e8eb7926bfdf9248bd0aa4cd73afa69
ponpon <ponponumi@gmail.com>
2024-03-23 18:44:57 +0900

    内部リンクかどうかを判定するメソッドを作成

--- src/Domain.php
+++ src/Domain.php
@@ -18,4 +18,26 @@ class Domain{
 
     return $host;
   }
+
+  public static function internalLinkCheck(string $url,string $internal_link=""){
+    // 内部リンクかどうかを判定する
+    // 内部リンクならtrue、外部リンクならfalseを返す
+    $url_host = self::hostGet($url);
+
+    if($internal_link !== ""){
+      // 内部のURLがあれば
+      $internal_host = self::hostGet($internal_link);
+    }else{
+      // 内部のURLがなければ
+      $internal_host = $_SERVER["HTTP_HOST"];
+    }
+
+    if($internal_host === $url_host){
+      // 内部リンクであれば
+      return true;
+    }else{
+      // 外部リンクであれば
+      return false;
+    }
+  }
 }
