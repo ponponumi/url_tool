562e1af6a08ebba70742aeeb7ba7ba506de0bd82
ponpon <ponponumi@gmail.com>
2024-03-23 18:31:59 +0900

    ホストを返すメソッドを作成

--- src/Domain.php
+++ src/Domain.php
@@ -3,5 +3,19 @@
 namespace Ponponumi\UrlTool;
 
 class Domain{
-  //
+  public static function hostGet(string $url,$port=true){
+    // URLからホストを取得する
+    $parse_url = parse_url($url);
+
+    $host = $parse_url["host"];
+
+    if($port){
+      if(array_key_exists("port",$parse_url)){
+        // ポートがあれば
+        $host .= ":" . $parse_url["port"];
+      }
+    }
+
+    return $host;
+  }
 }
