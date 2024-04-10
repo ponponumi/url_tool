cbd87ee90059f9bbe0baa45f66abc3ee1b3a9b3a
ponpon <ponponumi@gmail.com>
2024-04-11 00:42:45 +0900

    ページURLの取得テストを追加(ページ内リンク追加)

--- test/v1_0_1.php
+++ test/v1_0_1.php
@@ -8,3 +8,5 @@ var_dump(Domain::httpHostGet());
 var_dump(Domain::sslCheck());
 var_dump(Domain::topPageUrlGet());
 var_dump(Domain::pageUrlGet());
+var_dump(Domain::pageUrlGet("/index.php"));
+var_dump(Domain::pageUrlGet("index.php"));
